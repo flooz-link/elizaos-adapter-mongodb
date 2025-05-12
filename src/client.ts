f012c4ba2fcc6db54f5b6649c025585de976831c
DMalliaros <dimi.malliaros@gmail.com>
2025-05-12 13:32:49 +0200

    Add new content without Json

--- src/client.ts
+++ src/client.ts
@@ -461,13 +461,15 @@ class MongoDBDatabaseAdapter
         // );
         isUnique = similarMemories.length === 0;
       }
-      const content = JSON.stringify(memory.content);
       const createdAt = memory.createdAt ?? Date.now();
 
       await this.database.collection("memories").insertOne({
         id: memory.id ?? v4(),
         type: tableName,
-        content,
+        content: {
+          content: memory.content,
+          ...memory.content,
+        },
         embedding: memory.embedding ? Array.from(memory.embedding) : null,
         userId: memory.userId,
         roomId: memory.roomId,
